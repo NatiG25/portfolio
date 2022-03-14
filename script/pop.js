f78e2e1400fce3b51452be4156a9d2f14e66718a
NatiG <nati.g5684@gmail.com>
2022-03-15 00:33:46 +0300

    Change to AwesomeBooks

--- script/pop.js
+++ script/pop.js
@@ -17,8 +17,8 @@ const projects = [
     seeSourceLink: '#',
   },
   {
-    title: 'Tonic',
-    src: './image/Snapshoot-3Portfolio.svg',
+    title: 'Awesome Books',
+    src: './image/AwesomeBooks.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     tags: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
     seeLivelink: '#',
