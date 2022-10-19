39c3e540131e7768cf296a01fd03f9895cc99ede
NatiG <nati.g5684@gmail.com>
2022-10-19 07:54:07 +0300

    Add a new project MovieSpace

--- script/pop.js
+++ script/pop.js
@@ -4,7 +4,7 @@ const projects = [
     src: './image/Riyadh-portifolio.png',
     description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea"],
     tags: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
-    seeLivelink: '#',
+    seeLivelink: 'https://natig25.github.io/Riyadh-season/',
     seeSourceLink: '#',
   },
 
@@ -25,11 +25,11 @@ const projects = [
     seeSourceLink: '#',
   },
   {
-    title: 'Multi-Post Stories',
+    title: 'MovieSpace',
     src: './image/Snapshoot-4Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     tags: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
-    seeLivelink: '#',
+    seeLivelink: 'https://natig25.github.io/MovieSpace/',
     seeSourceLink: '#',
   },
 ];
