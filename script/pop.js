72932096765165c6526ab199e69bb31ab4bbb1d8
NatiG <nati.g5684@gmail.com>
2022-10-21 23:41:09 +0300

    Add MovieSpace as the last project in index.html

--- script/pop.js
+++ script/pop.js
@@ -26,11 +26,11 @@ const projects = [
   },
   {
     title: 'MovieSpace',
-    src: './image/Snapshoot-4Portfolio.svg',
+    src: './image/MovieSpace.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     tags: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
     seeLivelink: 'https://natig25.github.io/MovieSpace/',
-    seeSourceLink: '#',
+    seeSourceLink: 'https://github.com/NatiG25/MovieSpace',
   },
 ];
 
