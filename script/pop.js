9ccfe0370c7cac6a190f0a69cbe329759cc649db
NatiG <nati.g5684@gmail.com>
2022-10-21 23:11:04 +0300

    Add budget app project into the list of projects

--- script/pop.js
+++ script/pop.js
@@ -9,12 +9,12 @@ const projects = [
   },
 
   {
-    title: 'To Do List',
+    title: 'Budget App',
     src: './image/Todolist.png',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    tags: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstrap'],
-    seeLivelink: '#',
-    seeSourceLink: '#',
+    description: "You have a list of transactions associated with a category so that you can see how much money you spent and on what.",
+    tags: ['html', 'css', 'github', 'ruby', 'ruby on rails', 'Bootstrap'],
+    seeLivelink: 'https://budget-app-nati.herokuapp.com/',
+    seeSourceLink: 'https://github.com/NatiG25/budget-app',
   },
   {
     title: 'Awesome Books',
