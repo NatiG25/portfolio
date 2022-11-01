cee48e7a4de2b11eeb07757784c93e2d5f196a06
NatiG <nati.g5684@gmail.com>
2022-11-01 06:10:34 +0300

    Implement the menu app

--- script/script.js
+++ script/script.js
@@ -1,15 +1,30 @@
 const menu = document.querySelector('.menu');
 const info = document.querySelector('.info');
 const headline = document.querySelector('.background');
+const navClose = document.getElementById('nav_close'),
+navMenu = document.getElementById('nav_menu'),
+menuApp = document.getElementById('menu-app');
 
-menu.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  info.classList.toggle('active');
-  headline.classList.toggle('active');
-});
+const addShowClass = () => {
+  navMenu.classList.add('show-menu')
+}
+menuApp.addEventListener('click', addShowClass)
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu')
+})
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  menu.classList.remove('active');
-  info.classList.remove('active');
-  headline.classList.remove('active');
+document.querySelectorAll('.nav-mobile-link').forEach((n) => n.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu')
 }));
+
+// menu.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   info.classList.toggle('active');
+//   headline.classList.toggle('active');
+// });
+
+// document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+//   menu.classList.remove('active');
+//   info.classList.remove('active');
+//   headline.classList.remove('active');
+// }));
